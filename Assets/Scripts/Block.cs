e7d87d2466cf180894ca53afd720f83d555e36e4
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-04-14 08:55:08 -0700

    Block movement WIP
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // Start is called before the first frame update
+  void Start()
+  {
+
+    InvokeRepeating("Fall", 2.0f, 0.5f);
+      
+  }
+
+    // Update is called once per frame
+  void Update()
+  {
+      
+  }
+
+  void Fall() {
+
+    if(transform.localPosition.y < -17) {
+      Debug.Log("PLACED");
+      CancelInvoke();
+      return;
+    }
+
+    transform.Translate(0, -1f, 0);
+    Debug.Log(transform.localPosition.y);
+
+
+  }
+
+}
