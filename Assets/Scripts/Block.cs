84711cc14bae3ac697936d0f6bb6fde3dfc328d0
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-04-16 07:55:35 -0700

    Add Tetromino player movement
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
   public BoxCollider2D bc;
+  public float delay = 1.0f;
+
+  private float fall;
 
   void Start()
   {
     bc = GetComponent<BoxCollider2D>();
-    InvokeRepeating("Fall", 1.0f, 0.2f);
+    // InvokeRepeating("Fall", 1.0f, 0.2f);
   }
 
   void Update()
   {
+    // User input
+
+    if(Input.GetKeyDown(KeyCode.RightArrow)) {
+      transform.position += new Vector3(1, 0, 0);
+    } else if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+      transform.position += new Vector3(-1, 0, 0);
+    } else if(Input.GetKeyDown(KeyCode.UpArrow)) {
+      transform.Rotate(0, 0, 90);
+    } else if(Input.GetKeyDown(KeyCode.DownArrow) || Time.time > fall + delay) {
+      transform.position += new Vector3(0, -1, 0);
+      fall = Time.time;
+    }
       
   }
 
