99602dcddaa81bd9fb281ed40619b6f449ed970b
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-04-16 08:28:50 -0700

    Restrict player movement to grid
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,42 +4,43 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-  public BoxCollider2D bc;
   public float delay = 1.0f;
 
   private float fall;
+  private Game gameScript;
 
-  void Start()
-  {
-    bc = GetComponent<BoxCollider2D>();
-    // InvokeRepeating("Fall", 1.0f, 0.2f);
+  void Start() {
+    gameScript = FindObjectOfType<Game>();
   }
 
-  void Update()
-  {
-    // User input
-
+  void Update() {
     if(Input.GetKeyDown(KeyCode.RightArrow)) {
       transform.position += new Vector3(1, 0, 0);
+      if(!IsValid()) {
+        transform.position += new Vector3(-1, 0, 0);
+      }
     } else if(Input.GetKeyDown(KeyCode.LeftArrow)) {
       transform.position += new Vector3(-1, 0, 0);
+      if(!IsValid()) {
+        transform.position += new Vector3(1, 0, 0);
+      }
     } else if(Input.GetKeyDown(KeyCode.UpArrow)) {
       transform.Rotate(0, 0, 90);
     } else if(Input.GetKeyDown(KeyCode.DownArrow) || Time.time > fall + delay) {
       transform.position += new Vector3(0, -1, 0);
       fall = Time.time;
-    }
-      
+      if(!IsValid()) {
+        transform.position += new Vector3(0, 1, 0);
+      }
+    }      
   }
 
-  void Fall() {
-    if(transform.localPosition.y < -17) {
-      Debug.Log("PLACED");
-      CancelInvoke();
-      return;
+  bool IsValid() {
+    foreach(Transform mino in transform) {
+      if(!gameScript.IsInsideGrid(mino.position)) {
+        return false;
+      }
     }
-
-    transform.Translate(0, -1f, 0);
-    Debug.Log(transform.localPosition.y);
+    return true;
   }
 }
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+  public static int gridWidth = 10;
+  public static int gridHeight = 20;
+
+  void Start() {
+      
+  }
+
+  void Update() {
+      
+  }
+
+  public bool IsInsideGrid(Vector2 pos) {
+    return ((int)pos.x >= 0 && (int)pos.x < gridWidth & (int)pos.y >= 0);
+  }
+}
